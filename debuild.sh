083d0c0c452493af47c0f14eecb507b589b49a31
Junichi Uekawa <dancer@dancer64.netfort.gr.jp>
2007-01-06 15:31:42 +0900

    change git option.

--- debuild.sh
+++ debuild.sh
@@ -1,5 +1,5 @@
 # run debuild with .git ignore.
 debuild -us -uc -i.git
 git push --tags ssh://git.alioth.debian.org/git/collab-maint/binfmtc.git
-git push  ssh://git.alioth.debian.org/git/collab-maint/binfmtc.git
+git push --all ssh://git.alioth.debian.org/git/collab-maint/binfmtc.git
 
