208f15d61e0a1083fcd345996eebadca8b1b37c6
Junichi Uekawa <dancer@dancer64.netfort.gr.jp>
2007-07-14 18:03:06 +0900

    ignore kernel test within chroot

--- tests/realksh.sh
+++ tests/realksh.sh
@@ -4,4 +4,9 @@
 # requires: sudo, and a working kernel build directory enough to build
 # a kernel module, with an interface similar to linux 2.6.14-
 
-echo 'printk("Hello world\n");' | sudo ${srcdir}/realksh.c | grep ^KMSG:
+if [ -d /lib/modules/$(uname -r)/build ]; then
+    echo 'printk("Hello world\n");' | sudo ${srcdir}/realksh.c | grep ^KMSG:
+else
+    # ignore this test if it's within a chroot, or without access to kernel tree.
+    exit 77
+fi
